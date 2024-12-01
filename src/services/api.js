f034d1c8b42882b4e57e14d0e08be00ea50aa706
Krista Kaza <krikaz50070@stud.noroff.no>
2024-12-02 00:50:10 +0200

    fixes

--- src/services/api.js
+++ src/services/api.js
@@ -3,19 +3,11 @@ const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export async function fetchAllVenues() {
   try {
     const response = await fetch(`${BASE_URL}/venues`);
-
-    // Check if the response is JSON, or handle errors
-    const contentType = response.headers.get("Content-Type");
     if (!response.ok) {
-      throw new Error(`Failed to fetch venues, status: ${response.status}`);
+      throw new Error("Failed to fetch venues");
     }
-    if (!contentType || !contentType.includes("application/json")) {
-      throw new Error("Expected JSON response, but got HTML instead.");
-    }
-
     const data = await response.json();
-    console.log("API response data:", data);
-    return data.data;
+    return data;
   } catch (error) {
     console.error("Error fetching venues:", error);
     return [];
